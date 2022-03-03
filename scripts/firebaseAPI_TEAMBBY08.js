011608fefd929eeb6360d11db54c9868189993ee
Betty Nguyen <bettyanhn@gmail.com>
2022-03-02 16:48:42 -0800

    added firebase API key to gitignore

--- scripts/firebaseAPI_TEAMBBY08.js
+++ scripts/firebaseAPI_TEAMBBY08.js
@@ -2,13 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-  apiKey: "AIzaSyD80tS_NCx8UlpLYtVIIzn_bzAZOJDpkb0",
-  authDomain: "bby08---1800.firebaseapp.com",
-  projectId: "bby08---1800",
-  storageBucket: "bby08---1800.appspot.com",
-  messagingSenderId: "440589533137",
-  appId: "1:440589533137:web:f7f91c2fed0b2960ceeaf0",
-  measurementId: "G-XP6CSYX86F"
+  apiKey: "AIzaSyAEs69O8IIyjH08j9oG5uHVSrMtECRVGXA",
+  authDomain: "comp1800-bby08-33ae1.firebaseapp.com",
+  projectId: "comp1800-bby08-33ae1",
+  storageBucket: "comp1800-bby08-33ae1.appspot.com",
+  messagingSenderId: "1055165643126",
+  appId: "1:1055165643126:web:6298ce5151d571b328a257"
 };
 
 //--------------------------------------------
